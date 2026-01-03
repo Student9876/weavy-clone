7b267f66aab48479df39672db04f5947a5d32785
student9876 <shouvik9876@gmail.com>
2026-01-03 19:14:40 +0530

    Remove grid pattern

--- src/components/marketing/HeroSection.tsx
+++ src/components/marketing/HeroSection.tsx
@@ -1,9 +1,9 @@
 import HeroWorkflow from "@/components/marketing/HeroWorkflow";
 export default function HeroSection() {
 	return (
-		<section className=" relative min-h-[120vh] flex flex-col pt-32 px-6 overflow-hidden">
+		<section className="relative min-h-[120vh] flex flex-col pt-32 px-6 overflow-hidden">
 			{/* Background Grid */}
-			<div className="absolute inset-0 bg-[linear-gradient(to_right,#e5e5e5_1px,transparent_1px),linear-gradient(to_bottom,#e5e5e5_1px,transparent_1px)] bg-size-[16px_16px] pointer-events-none opacity-60" />
+			{/* <div className="absolute inset-0 bg-[linear-gradient(to_right,#e5e5e5_1px,transparent_1px),linear-gradient(to_bottom,#e5e5e5_1px,transparent_1px)] bg-size-[16px_16px] pointer-events-none opacity-60" /> */}
 
 			{/* Typography */}
 			<div className="w-full z-10 mx-10 grid grid-cols-12 gap-8 relative mb-30">
@@ -21,7 +21,7 @@ export default function HeroSection() {
 			{/* --- INTERACTIVE WORKFLOW CONTAINER --- */}
 			<div className="relative w-full h-[80vh] mx-auto max-w-450 rounded-t-[40px] overflow-hidden border-t border-x border-black/5 shadow-[0_-20px_60px_rgba(0,0,0,0.03)]">
 				{/* The Gradient Background */}
-				<div className="absolute inset-0 bg-linear-to-b from-white via-[#faf9f6] to-[#f0efe9] z-0" />
+				{/* <div className="absolute inset-0 bg-linear-to-b from-white via-[#faf9f6] to-[#f0efe9] z-0" /> */}
 
 				{/* The React Flow Instance */}
 				<div className="absolute inset-0 z-10">
